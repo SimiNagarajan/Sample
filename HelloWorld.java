67c015f8215579ef187bf26759e78815d93e5076
SimiNagarajan <simi.gct@gmail.com>
2016-12-27 11:21:04 +0530

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,16 @@
+package HelloWorld;
+
+public class HelloWorld {
+
+	public static void main(String args[])
+	{
+		System.out.println("Welcome to 2017");
+		SayHello("Simi");
+		
+	}
+
+	public  static void SayHello(String name) {
+		System.out.println("Hello"+" " +name);
+	}
+	
+}

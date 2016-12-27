aa081cd7ada79b8862b2cb9dbb2095d44c9909bf
SimiNagarajan <simi.gct@gmail.com>
2016-12-27 11:42:00 +0530

    Add files via upload

--- HelloWorld.java
+++ HelloWorld.java
@@ -10,7 +10,7 @@ public class HelloWorld {
 	}
 
 	public  static void SayHello(String name) {
-		System.out.println("Hello"+" " +name);
+		System.out.println("Hello Welcome!!!!"+" " +name);
 	}
 	
 }
